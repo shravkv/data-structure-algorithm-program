d3bf03b779be2d306471a660423be64daa2c066b
shravkv <sravan.k.vodnala@gmail.com>
2022-06-27 22:55:01 +0530

    Added: Finding prime numbers from 1 to 1000

---
+++ src/com/bridgelabz/BinarySearch.java
@@ -0,0 +1,31 @@
+package com.bridgelabz;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to BinarySearch Program");
+        BinarySearch search = new BinarySearch();
+        String[] array = {"angel", "banana", "camel", "delhi"};
+        String word = "angel";
+        int result = search.searchingMethod(array, word);
+        if (result == -1)
+            System.out.println("Element not found in the data");
+        else
+            System.out.println("Element found in the data at Index : " + result);
+    }
+
+    public int searchingMethod(String[] array, String word) {
+        int left = 0, right = array.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (word.compareTo(array[mid]) == 0) {
+                return mid;
+            } else if (word.compareTo(array[mid]) > 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/com/bridgelabz/PrimeNumbers.java
@@ -0,0 +1,32 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class PrimeNumbers {
+
+    public static void main(String[] args) {
+        PrimeNumbers prime = new PrimeNumbers();
+        prime.primeMethod();
+    }
+
+    public void primeMethod() {
+        System.out.println("PrimeNumbers are :");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int a = scanner.nextInt();
+        System.out.println("Enter the last number");
+        int b = scanner.nextInt();
+        for (int i = a; i < b; i++) {
+            int count = 0;
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+}
