6f0e9b0c436bc0022151d8dd34e3bef760ae24b8
shravkv <sravan.k.vodnala@gmail.com>
2022-07-03 11:34:44 +0530

    Added: Sorting the array using Insertion search

---
+++ src/com/bridgelabz/InsertionSort.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to InsertionOrder program");
+        String[] array = {"King", "Camel", "Xerox", "Maruti", "Fox"};
+        String[] sorted = insertionMethod(array);
+        System.out.println("Sorting Array is :");
+        for (String s : sorted) {
+            System.out.println(s);
+        }
+    }
+    public static String[] insertionMethod(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i].compareTo(array[j]) > 0) {
+                    String temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
